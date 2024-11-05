4f4fcf242bb23199137bc3987f50e104851a86b9
ifuran <silalahifran@gmail.com>
2024-11-05 19:47:30 +0700

    feat: update text when cart empty

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -24,52 +24,61 @@ const CartPage = () => {
     <>
       <section className="container cart-wrapper pt-5 mt-5">
         <h3 className="cart-title text-success">My Cart</h3>
-        <table className="table mt-3">
-          <thead>
-            <tr>
-              <th scope="col">Product</th>
-              <th scope="col">Price</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((product) => (
-              <tr key={product.id}>
-                <td className="w-50">
-                  <Link to={`/product/${product.id}`} className="text-dark">
-                    {product.title}
-                  </Link>
-                </td>
-                <td>{product.price}</td>
-                <td>
-                  <div className="cart-product-quantity">
-                    <button
-                      className="btn btn-sm btn-success rounded"
-                      onClick={() => plusOneToCart(product)}
-                    >
-                      +
-                    </button>
-                    <span className="mx-3">{product.qty}</span>
-                    <button
-                      className="btn btn-sm btn-success rounded"
-                      onClick={() => minusOneFromCart(product)}
-                    >
-                      -
-                    </button>
-                  </div>
-                </td>
-                <td>{product.price * product.qty}</td>
+        {data.length === 0 ? (
+          <div className="text-center py-5">
+            <p>Cart is empty</p>
+            <Link to="/" className="text-success">
+              See Products
+            </Link>
+          </div>
+        ) : (
+          <table className="table mt-3">
+            <thead>
+              <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total</th>
               </tr>
-            ))}
-            <tr>
-              <td colSpan="3" className="fw-bold text-center">
-                Total
-              </td>
-              <td>${price}</td>
-            </tr>
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((product) => (
+                <tr key={product.id}>
+                  <td className="w-50">
+                    <Link to={`/product/${product.id}`} className="text-dark">
+                      {product.title}
+                    </Link>
+                  </td>
+                  <td>{product.price}</td>
+                  <td>
+                    <div className="cart-product-quantity">
+                      <button
+                        className="btn btn-sm btn-success rounded"
+                        onClick={() => plusOneToCart(product)}
+                      >
+                        +
+                      </button>
+                      <span className="mx-3">{product.qty}</span>
+                      <button
+                        className="btn btn-sm btn-success rounded"
+                        onClick={() => minusOneFromCart(product)}
+                      >
+                        -
+                      </button>
+                    </div>
+                  </td>
+                  <td>{product.price * product.qty}</td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="3" className="fw-bold text-center">
+                  Total
+                </td>
+                <td>${price}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
       </section>
     </>
   );
